f6022649747f11fe4c57599704198cc8409a1e42
TheChriggu <60545256+TheChriggu@users.noreply.github.com>
2020-07-11 14:28:32 +0200

    Added LoginStuff

---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+using System;
+
+public class Login : MonoBehaviourPunCallbacks
+{
+    string gameVersion = "1";
+    public Text userName;
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    public void OnLogin()
+    {
+        string name = userName.text;
+
+        if (name != "")
+        {
+            PhotonNetwork.NickName = name;
+            Connect();
+        }
+    }
+
+    void Connect()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinLobby();
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gameVersion;
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        TryCreateNewRoom();
+    }
+
+    void TryCreateNewRoom()
+    {
+        RoomOptions options = new RoomOptions { MaxPlayers = 2 };
+        PhotonNetwork.CreateRoom(null, options, null);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        TryCreateNewRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            SceneManager.LoadScene("WaitingRoom");
+        }
+    }
+}
---
+++ Assets/WaitingRoomTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class WaitingRoomTest : MonoBehaviour
+{
+    public Text masterName;
+    public Text clientName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        masterName.text = PhotonNetwork.MasterClient.NickName;
+
+        var playerlist = PhotonNetwork.PlayerList;
+
+        foreach (var player in playerlist)
+        {
+            if (player.NickName != masterName.text)
+            {
+                clientName.text = player.NickName;
+            }
+        }
+    }
+}
