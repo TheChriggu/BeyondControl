b27e1cc48a1c93d69de307765674e6049a914971
Pyrodar <68148598+Pyrodar@users.noreply.github.com>
2020-07-11 18:35:55 +0200

    Added SpeedModifiers
    Added Boosters and different floortypes to affect playerMovement
    OnTriggerExit() not working for Streets and Grass

---
+++ Assets/Phil_Scripts/Booster.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Booster : MonoBehaviour
+{
+    public float boostingPower = 100;
+    public bool playerStartsSpinning = false;
+}
