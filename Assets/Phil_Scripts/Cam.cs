88218db87d89e9aa656058445b4e74dbb02f31f1
TheChriggu <60545256+TheChriggu@users.noreply.github.com>
2020-07-12 13:53:03 +0200

    Replaced graphics, improved camera

--- Assets/Phil_Scripts/Cam.cs
+++ Assets/Phil_Scripts/Cam.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Cam : MonoBehaviour
 {
@@ -6,10 +7,47 @@ public class Cam : MonoBehaviour
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
-    void FixedUpdate()
+    public float levelStartViewSpeed = 0.1f;
+
+    bool followPlayer = false;
+    bool allowCameraMovement = false;
+
+    private void Start()
+    {
+        followPlayer = false;
+        allowCameraMovement = false;
+        StartCoroutine(StartUp());
+    }
+
+    private IEnumerator StartUp()
+    {
+        yield return new WaitForSeconds(0.5f);
+        allowCameraMovement = true;
+    }
+
+    void Update()
     {
         Vector3 finalPosition = player.transform.position + offset;
-        Vector3 currentPosition = Vector3.Lerp(transform.position, finalPosition, smoothSpeed);
+        Vector3 currentPosition = transform.position;
+
+        if (followPlayer)
+        {
+            Debug.Log("Cam lerps");
+            currentPosition = Vector3.Lerp(transform.position, finalPosition, smoothSpeed);
+        }
+        else if(allowCameraMovement)
+        {
+            Debug.Log("Cam moves");
+            var direction = finalPosition - transform.position;
+            direction = direction.normalized;
+
+            currentPosition = transform.position + direction * levelStartViewSpeed;
+
+            if((currentPosition - finalPosition).sqrMagnitude < 0.01f)
+            {
+                followPlayer = true;
+            }
+        }
         transform.position = currentPosition;
     }
 }
